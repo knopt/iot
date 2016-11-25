c1b5808872cd25aeee4886d51ea8fd6654ba2e83
knopt <tomasz.knopik@gmail.com>
2016-11-25 23:04:46 +0100

    initial works with angular examples

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
-import '../../public/css/styles.css';
 @Component({
   selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>{{title}}</h1>
+    <a routerLink="/heroes">Heroes</a>
+    <router-outlet></router-outlet>
+  `
 })
-export class AppComponent { }
+export class AppComponent {
+  title = 'Tour of Heroes';
+}
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,13 +1,45 @@
-import { NgModule } from '@angular/core';
+import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
-import { AppComponent } from './app.component';
+import { FormsModule }    from '@angular/forms';
+import { RouterModule }   from '@angular/router';
+
+import { AppComponent }        from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { DashboardComponent}   from './dashboard.component';
+import { HeroesComponent }     from './heroes.component';
+import { HeroService }         from './hero.service';
+
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+     {
+       path: 'heroes',
+       component: HeroesComponent
+     },
+     {
+       path: 'dashboard',
+       component: DashboardComponent
+     },
+     {
+       path: '',
+       redirectTo: '/dashboard',
+       pathMatch: 'full'
+     }
+   ])
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent
+  ],
+  providers: [
+    HeroService
   ],
   bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+
+export class AppModule {
+}
